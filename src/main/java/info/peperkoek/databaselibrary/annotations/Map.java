19320cec213fb0fdc101a38832a836703390a149
REXOTIUM <rick.pie@hotmail.com>
2017-06-20 17:58:49 +0200

    Add map annotation and databaseexception

---
+++ src/main/java/info/peperkoek/databaselibrary/annotations/Map.java
@@ -0,0 +1,14 @@
+package info.peperkoek.databaselibrary.annotations;
+
+import java.lang.annotation.*;
+
+/**
+ *
+ * @author Rick Pijnenburg - REXOTIUM
+ * @email m.a.a.pijnenburg@gmail.com
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Map {
+    String mapping();
+}
---
+++ src/main/java/info/peperkoek/databaselibrary/exceptions/DatabaseException.java
@@ -0,0 +1,11 @@
+package info.peperkoek.databaselibrary.exceptions;
+
+/**
+ *
+ * @author Rick Pijnenburg - REXOTIUM
+ * @email m.a.a.pijnenburg@gmail.com
+ */
+public class DatabaseException extends Exception {
+    private static final long serialVersionUID = 944789858777653985L;
+
+}
