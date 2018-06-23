f044fdd700a97e177b850f3a493aab5b63c5ee75
REXOTIUM <rick.pie@hotmail.com>
2018-06-23 17:50:54 +0200

    Update to fix issue found after use

--- src/main/java/info/peperkoek/databaselibrary/core/MSSQLDataAccessObject.java
+++ src/main/java/info/peperkoek/databaselibrary/core/MSSQLDataAccessObject.java
@@ -38,7 +38,7 @@ class MSSQLDataAccessObject extends DataAccessObject {
         columns.delete(columns.length() - 2, columns.length()); //remove excess comma
         values.delete(values.length() - 2, values.length()); //remove excess comma
         if(pkAutoGen) {
-            String sql = String.format(INSERT_ITEM_OUTPUT_MSSQL, table, columns.toString(), DBUtils.getPrimaryKey(obj), values.toString());
+            String sql = String.format(INSERT_ITEM_OUTPUT_MSSQL, table, columns.toString(), DBUtils.getPrimaryKeyName(obj.getClass()), values.toString());
             if(!DBUtils.hasLinkTable(obj.getClass()))
                 return insertQuery(obj, sql, true);
             return insertQuery(obj, sql, true) && insertLinkTable(obj);
